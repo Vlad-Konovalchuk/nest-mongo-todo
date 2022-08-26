465612ffcacd9babe70099c4ba8cac257f42b4d5
Vlad Konovalchuk <vkonovalchuk@lohika.com>
2022-08-26 23:24:36 +0300

    add todo schema

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,14 @@
+import { Todo, TodoSchema } from './schemas/todo.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
 
 @Module({
   providers: [TodoService],
-  controllers: [TodoController]
+  controllers: [TodoController],
+  imports: [
+    MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }]),
+  ],
 })
 export class TodoModule {}

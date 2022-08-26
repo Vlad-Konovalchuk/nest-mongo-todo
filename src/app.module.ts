4bb3ad7513c9ece7c8bee17e4a6f3ba32239e218
Vlad Konovalchuk <vkonovalchuk@lohika.com>
2022-08-26 23:24:10 +0300

    add mongoose package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, MongooseModule.forRoot(process.env.MONGODB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })

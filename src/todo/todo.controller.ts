d3e844992ace8883719a3775853fec028cc486a0
Vlad Konovalchuk <vkonovalchuk@lohika.com>
2022-09-01 21:54:36 +0300

    add todo controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { TodoService } from './todo.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Put,
+  Post,
+} from '@nestjs/common';
 
-@Controller('todo')
-export class TodoController {}
+@Controller('todos')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async getAll() {
+    return await this.todoService.findAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return await this.todoService.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() createTodoDto: CreateTodoDto) {
+    return await this.todoService.create(createTodoDto);
+  }
+
+  @Put('id')
+  async update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+    return await this.todoService.update(id, updateTodoDto);
+  }
+
+  @Delete('id')
+  async delete(@Param('id') id: string) {
+    return await this.todoService.delete(id);
+  }
+}

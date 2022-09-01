257a0b560448acef020e8dc2e2f87d6c0ea24a6e
Vlad Konovalchuk <vkonovalchuk@lohika.com>
2022-09-01 21:53:37 +0300

    add config and mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
+import { envValidation } from './config/envValidation';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
-
 @Module({
-  imports: [TodoModule, MongooseModule.forRoot(process.env.MONGODB_URI)],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidation,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          uri: configService.get('MONGODB_URI'),
+        };
+      },
+      inject: [ConfigService],
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

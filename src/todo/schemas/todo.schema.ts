f4b65b87cee5385c169e570fc7070b1814a3e028
Vlad Konovalchuk <vkonovalchuk@lohika.com>
2022-09-01 21:53:07 +0300

    update schema

--- src/todo/schemas/todo.schema.ts
+++ src/todo/schemas/todo.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 export type TodoDocument = Todo & Document;
 
-@Schema()
+@Schema({ timestamps: true })
 export class Todo {
   @Prop({ required: true })
   title: string;
@@ -12,12 +12,6 @@ export class Todo {
 
   @Prop()
   completedAt?: Date;
-
-  @Prop({ required: true })
-  createdAt: Date;
-
-  @Prop()
-  deletedAt?: Date;
 }
 
 export const TodoSchema = SchemaFactory.createForClass(Todo);
